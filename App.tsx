b6f85e6aed9d4401c202bb7247e3d4f24e178988
teerryn <slaughterer90@gmail.com>
2019-07-13 11:52:04 +0100

    Load fonts before app startup

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import useExpoAssetLoader from '@kevinwolf/use-expo-asset-loader';
 import AppNavigator from './screens';
+import { AppLoading } from 'expo';
 
 const AppContainer = createAppContainer(AppNavigator);
 
+const fonts = {
+  Roboto: require('native-base/Fonts/Roboto.ttf'),
+  Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+};
+
 export default function App() {
+  const assetLoader = useExpoAssetLoader({ fonts });
+
+  if (!assetLoader.isReady) {
+    return <AppLoading {...assetLoader.getAppLoadingProps()} />;
+  }
+
   return <AppContainer />;
 }
